453f0fdf6b5ac995ee19783e7787c80f9a538fb1
aboobackersiddeeq <a.siddeeqpktr@gmail.com>
2023-04-12 13:00:31 +0530

    initial commit

---
+++ src/components/ToDoApp/AddTask.js
@@ -0,0 +1,73 @@
+// import { Alert } from 'react-bootstrap'
+
+import React, { useEffect, useRef, useState } from "react";
+const AddTask = ({ addTask, evalue, update }) => {
+  const inputRef = useRef(null);
+
+  const [values, addvalue] = useState(evalue !== "" ? evalue : "");
+  const additem = () => {
+    if (!values || /^\s*$/.test(values)) {
+      return;
+    } else {
+      addTask(values);
+      addvalue("");
+    }
+  };
+  const edititem = (e) => {
+    e.preventDefault();
+    update(values);
+    addvalue("");
+  };
+
+  const changevalue = (e) => {
+    addvalue(e);
+  };
+  useEffect(() => {
+    inputRef.current.focus();
+    inputRef.current.value = evalue;
+  });
+
+  return (
+    <div className="input-container">
+      {evalue === "" ? (
+        <>
+          <input
+            ref={inputRef}
+            type="text"
+            className="input"
+            placeholder="Add a new Task"
+            //  value={values}
+            value={values}
+            onChange={(e) => {
+              changevalue(e.target.value);
+            }}
+          />
+
+          <button onClick={additem} className="add-btn" type="">
+            ADD
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            ref={inputRef}
+            type="text"
+            className="input"
+            placeholder="Update your item"
+            //  value={values}
+            value={values}
+            onChange={(e) => {
+              changevalue(e.target.value);
+            }}
+          />
+
+          <button onClick={edititem} className="add-btn" type="">
+            EDIT
+          </button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default AddTask;
